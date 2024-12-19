9fae6cf6da3ab82eac605900b4f4bc603bb7214d
Jesus <jjcorrales76@gmail.com>
2024-12-19 09:02:06 -0700

    changes

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-let navbar = document.getElementById('navbar')
-//console.log('navbar')
-let div1 = document.querySelector('div1')
+const fruits = ["apples", "oranges", "mango", 'banana', "kiwi", "pineapple"];
+const navbar = document.querySelector('#navbar');
+navbar.style.backgroundColor = 'var(--navbar';
+navbar.style.height = "100%";
+//console.log(navbar)
+
+const anchorElement = document.querySelectorAll("a");
+for (let i = 0; i < anchorElement.length; i++) {
+    const anchor = anchorElement[i];
+    console.log(anchor)
+}
+const listItem = document.createElement("li");
+const myList = document.getElementById("div1");
+function addListItem() {
+    for (let i = 0; i < 1; i++) {
+        const listItem = document.createElement("li");
+        listItem.textContent = fruits[i];
+        const myList = document.getElementById("div1");
+        myList.appendChild(listItem)
+    }
+}
+addListItem()
+
+const generateButton = document.getElementById("generateButton")
+function generateRandomWord() {
+    const randomIndex = Math.floor(Math.random() * fruits.length);
+    const randomWord = fruits[randomIndex];
+    const listItem2 = document.createElement("li");
+    const myList2 = document.getElementById("div1");
+    listItem2.textContent = randomWord;
+    myList2.appendChild(listItem2)
+}
+generateButton.addEventListener("click", generateRandomWord);
+
+;
