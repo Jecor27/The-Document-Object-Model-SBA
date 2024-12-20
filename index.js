f3cea9ca8d6df4960f7fff4897d2bfed6374cbf1
Jesus <jjcorrales76@gmail.com>
2024-12-19 19:50:40 -0700

    some more changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fruits = ["apples", "oranges", "mango", 'banana', "kiwi", "pineapple"];
+const jokes = ["apples", "oranges", "mango", 'banana', "kiwi", "pineapple"];
 const navbar = document.querySelector('#navbar');
 navbar.style.backgroundColor = 'var(--navbar';
 navbar.style.height = "100%";
@@ -9,27 +9,32 @@ for (let i = 0; i < anchorElement.length; i++) {
     const anchor = anchorElement[i];
     console.log(anchor)
 }
-const listItem = document.createElement("li");
-const myList = document.getElementById("div1");
-function addListItem() {
-    for (let i = 0; i < 1; i++) {
-        const listItem = document.createElement("li");
-        listItem.textContent = fruits[i];
-        const myList = document.getElementById("div1");
-        myList.appendChild(listItem)
-    }
-}
-addListItem()
+// const listItem = document.createElement("li");
+const myList = document.querySelector("#div1");
+// function addListItem() {
+//     for (let i = 0; i < 0; i++) {
+//         const listItem = document.createElement("li");
+//         listItem.textContent = fruits[i];
+//         const myList = document.getElementById("div1");
+//         myList.appendChild(listItem)
+//     }
+// }
+// addListItem()
 
-const generateButton = document.getElementById("generateButton")
+
+const generateButton = document.getElementById("generateButton");
+const resetButton = document.getElementById("reset-btn");
+const para = document.querySelector("p");
+
 function generateRandomWord() {
-    const randomIndex = Math.floor(Math.random() * fruits.length);
-    const randomWord = fruits[randomIndex];
-    const listItem2 = document.createElement("li");
-    const myList2 = document.getElementById("div1");
-    listItem2.textContent = randomWord;
-    myList2.appendChild(listItem2)
+
+    const randomIndex = Math.floor(Math.random() * jokes.length);
+
+    const randomWord = jokes[randomIndex];
+
+    para.textContent = `Random Word: ${randomWord}`;
+
 }
 generateButton.addEventListener("click", generateRandomWord);
 
-;
+
