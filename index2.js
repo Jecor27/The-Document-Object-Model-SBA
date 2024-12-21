c84a59571242d286037cf2cb680e2afbc553a48e
Jesus <jjcorrales76@gmail.com>
2024-12-20 19:48:35 -0700

    changed the order of pages

--- index2.js
+++ index2.js
@@ -1,27 +1,59 @@
-const redbutton = document.getElementById("generateButton2");
-const L = document.querySelector(".label");
-t = document.getElementById("choose")
-//console.log(t)
-//console.log(L)
+//alert("WELCOME!");
+const jokes = ["apples", "oranges", "mango", 'banana', "kiwi", "pineapple", "apples1", "oranges2", "mango1", 'banana1', "kiwi1", "pineapple1"];
 
-redbutton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const labelText = t.value;
+const links = [
+    { text: 'Want More?', href: 'https://www.nature.org/en-us/about-us/where-we-work/united-states/washington/stories-in-washington/dad-jokes-so-bad-youll-want-to-make-like-a-tree-and-leaf/'},
+
+    {
+        text: "I'm Useless, I just Reload 😞", href: 'index2.html'
+    },
+];
+
+const navbar = document.querySelector('#navbar');
+//console.log(navbar)
+navbar.setAttribute('style', "font-weight:bold");
+navbar.style.height = "100% !important";
+navbar.classList.add('flex-around');
+// const anchors = document.querySelectorAll('a');
+// console.log(anchors)
+//lastElement3.classList.add("right")
+
+//console.log(navbar)
 
 
-    if (labelText === "") {
-        window.location.href = './index2.html';
-        alert("Please answer the question!!!!! yes or no?");
-    } else if (labelText === "yes") {
-        window.location.href = './index.html';
+for (let i = 0; i < links.length; i++) {
+    let linkElement = document.createElement("a");
+    linkElement.setAttribute("href", links[i].href)
+    linkElement.textContent = (links[i].text);
+    navbar.appendChild(linkElement);
+    //console.log(lastElement);
+}//was setting the anchor tag and then appending it to the navbar
 
-    } else {
-        window.location.href = './index2.html';
-        alert("Well i guess you're stuck here...... Unless you input yes.")
-    }
-});
-redbutton.style.fontSize ="30px";
-L.style.fontSize = "20px"; // Increase the font size
-t.style.width = "500px"; // Set a specific width
-t.style.fontSize = "20px"; // Increase the font size
-t.style.padding = "10px"; //padding for more space
+const allAnchors = navbar.querySelectorAll("a");
+const lastAnchor = allAnchors[allAnchors.length - 1];
+lastAnchor.setAttribute('class', 'right')
+//console.log("Last anchor tag:", lastAnchor);
+
+const myList = document.querySelector("#div1");
+const firstElement = myList.firstElementChild;
+//console.log(firstElement);
+const generateButton = document.getElementById("generateButton");
+const resetButton = document.getElementById("reset-btn");
+const para = document.querySelector("p");
+
+function generateRandomWord() {
+
+    const randomIndex = Math.floor(Math.random() * jokes.length);
+
+    const randomWord = jokes[randomIndex];
+
+    para.textContent = `Random Word: ${randomWord}`;
+
+}//the idea was to generate a random word as it pulls from the created array
+generateButton.addEventListener("click", generateRandomWord);
+
+resetButton.addEventListener ("click", () => {
+    let resetjokes = []
+    para.textContent = " ";
+    console.log("Jokes array reset:", jokes);
+})//need the reset button to reset the content of text everytime it was clicked
